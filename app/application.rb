9508f151c9b02322a1c7670a54c20ddeec5f83a6
Stephen Olson <stephenolson@gmail.com>
2019-08-04 00:51:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,7 +18,7 @@ class Application
       if @@cart.empty?
         resp.write "Your cart is empty."
       else
-        @@cart.each 
+        @@cart.each {
     
     else
       resp.write "Path Not Found"
