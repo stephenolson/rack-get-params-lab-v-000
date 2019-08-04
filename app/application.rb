3ab77ea88f5ea4aa2e5a73933c2f43a0cb21fca5
Stephen Olson <stephenolson@gmail.com>
2019-08-04 00:50:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart)
-    
+      if cart
     
     else
       resp.write "Path Not Found"
